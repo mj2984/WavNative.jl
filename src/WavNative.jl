98b2e8014c30e9fde6055dc622294d5effb20294
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-03 20:56:23 -0400

    Update WavNative.jl
    Consistency Updates.
    Consistently returns Samples and uses type dispatch consistently.

--- src/WavNative.jl
+++ src/WavNative.jl
@@ -2,24 +2,30 @@ module WavNative
 using BitIntegers, FixedPointNumbers, SamplesCore
 export WavMetadata, get_wav_layout, audioread
 
-struct WavMetadata{Bits, Channels}
-    format_tag::UInt16
+struct WavMetadata{T, Channels}
     sample_rate::Int
-    data_offset::Int64
-    data_size::Int64
+    data_offset::Int
+    total_frames::Int
 end
 
-const TransportMapping = Dict{Int, DataType}(8 => UInt8, 16 => Int16, 24 => Int32, 32 => Int32)
+@inline function get_transport_type(::Type{T}) where {T}
+    if T === Q0f15
+        return Int16
+    elseif T === Q0f23
+        return Int32
+    elseif T === Q0f31
+        return Int32
+    else
+        return T
+    end
+end
+
 const WavMemoryInput = Union{Vector{UInt8},Tuple{Vector{UInt8},Ptr{Cvoid}}}
 
 const RIFF_ID = (UInt8('R'), UInt8('I'), UInt8('F'), UInt8('F'))
 const WAVE_ID = (UInt8('W'), UInt8('A'), UInt8('V'), UInt8('E'))
 const FMT_ID  = (UInt8('f'), UInt8('m'), UInt8('t'), UInt8(' '))
 const DATA_ID = (UInt8('d'), UInt8('a'), UInt8('t'), UInt8('a'))
-
-get_nbits(::WavMetadata{Bits, Channels}) where {Bits, Channels} = Bits
-get_nchans(::WavMetadata{Bits, Channels}) where {Bits, Channels} = Channels
-get_base_type(meta::WavMetadata{Bits, Channels}) where {Bits, Channels} = meta.format_tag == 3 ? (Bits == 32 ? Float32 : Float64) : Bits == 16 ? Q0f15 : (Bits == 24 ? Q0f23 : Q0f31)
 
 function malloc_read(path::String)
     sz = filesize(path)
@@ -40,24 +46,27 @@ function get_layout_wav(data::AbstractVector{UInt8})
     length(data) < 44 && error("File too small to be a WAV")
     (ntuple(i -> data[i], 4) === RIFF_ID && ntuple(i -> data[i+8], 4) === WAVE_ID) || error("Not a RIFF/WAVE file")
     
-    fmt_tag::UInt16, Channels::UInt16, rate::UInt32, Bits::UInt16, data_offset::Int64, data_size::Int64 = 0, 0, 0, 0, 0, 0
-    pos::Int = 13
-    while pos + 8 <= length(data)
-        chunk_id = (data[pos], data[pos+1], data[pos+2], data[pos+3])
-        sz = UInt32(data[pos+4]) | (UInt32(data[pos+5]) << 8) | (UInt32(data[pos+6]) << 16) | (UInt32(data[pos+7]) << 24)
-        chunk_data = pos + 8
+    fmt_tag::UInt16, Channels::UInt16, rate::UInt32, Bits::UInt16, data_offset::Int64, total_frames::Int64 = 0, 0, 0, 0, 0, 0
+    pos_offset::Int = 12
+    while pos_offset + 8 < length(data)
+        chunk_id = (data[pos_offset+1], data[pos_offset+2], data[pos_offset+3], data[pos_offset+4])
+        sz = UInt32(data[pos_offset+5]) | (UInt32(data[pos_offset+6]) << 8) | (UInt32(data[pos_offset+7]) << 16) | (UInt32(data[pos_offset+8]) << 24)
+        chunk_data = pos_offset + 8
         if chunk_id === FMT_ID
-            fmt_tag  = UInt16(data[chunk_data]) | (UInt16(data[chunk_data+1]) << 8)
-            Channels = (UInt16(data[chunk_data+2]) | (UInt16(data[chunk_data+3]) << 8))
-            rate     = (UInt32(data[chunk_data+4]) | (UInt32(data[chunk_data+5]) << 8) | (UInt32(data[chunk_data+6]) << 16) | (UInt32(data[chunk_data+7]) << 24))
-            Bits     = (UInt16(data[chunk_data+14]) | (UInt16(data[chunk_data+15]) << 8))
+            fmt_tag  = UInt16(data[chunk_data+1]) | (UInt16(data[chunk_data+2]) << 8)
+            Channels = (UInt16(data[chunk_data+3]) | (UInt16(data[chunk_data+4]) << 8))
+            rate     = (UInt32(data[chunk_data+5]) | (UInt32(data[chunk_data+6]) << 8) | (UInt32(data[chunk_data+7]) << 16) | (UInt32(data[chunk_data+8]) << 24))
+            Bits     = (UInt16(data[chunk_data+15]) | (UInt16(data[chunk_data+16]) << 8))
         elseif chunk_id === DATA_ID
-            data_size, data_offset = Int64(sz), chunk_data
-            break 
+            (Channels == 0 || Bits == 0) && error("Invalid WAV: DATA chunk before FMT chunk")
+            total_frames = sz ÷ (Channels * (Bits ÷ 8))
+            data_offset = chunk_data
+            break
         end
-        pos = chunk_data + sz + (sz % 2)
+        pos_offset = chunk_data + sz + (sz % 2)
     end
-    return WavMetadata{Int(Bits), Int(Channels)}(fmt_tag, Int(rate), data_offset, data_size)
+    Dtype = fmt_tag == UInt16(3) ? (Bits == 32 ? Float32 : Float64) : Bits == 16 ? Q0f15 : (Bits == 24 ? Q0f23 : Q0f31)
+    return WavMetadata{Dtype, Int(Channels)}(Int(rate), data_offset, total_frames)
 end
 
 function get_layout_wav(path::String)
@@ -65,17 +74,24 @@ function get_layout_wav(path::String)
     return get_layout_wav(header_chunk)
 end
 
-function base_wav_parser(Input::WavMemoryInput, meta::WavMetadata{Bits, Channels}, ::Type{T}) where {Bits, Channels, T}
+@inline function is_bit_compatible(::Type{S}, ::Type{T}) where {S, T}
+    (S === Q0f23 || T === Q0f23) && return false 
+    sizeof(S) != sizeof(T) && return false
+    S === T && return true
+    return (S <: Union{FixedPoint, Integer}) && (T <: Union{FixedPoint, Integer})
+end
+
+function base_wav_parser(Input::WavMemoryInput, meta::WavMetadata{S, Channels}, ::Type{T}) where {S, Channels, T}
     data, raw_ptr, ismalloc = if Input isa Tuple
         Input[1], Input[2], true
     else
         Input, pointer(Input), false
     end
     TargetType = Sample{Channels, T}
-    n_frames = meta.data_size ÷ (Channels * (Bits ÷ 8))
+    n_frames = meta.total_frames
 
-    if Bits != 24 && sizeof(T) * 8 == Bits # FAST PATH: Zero-copy
-        audio_ptr = convert(Ptr{UInt8}, raw_ptr) + (meta.data_offset - 1)
+    if sizeof(S) != 3 && is_bit_compatible(S, T) # FAST PATH: Zero-copy
+        audio_ptr = convert(Ptr{UInt8}, raw_ptr) + (meta.data_offset)
         final_view = unsafe_wrap(Array, reinterpret(Ptr{TargetType}, audio_ptr), n_frames)
         if ismalloc
             finalizer(x -> Libc.free(raw_ptr), final_view)
@@ -85,14 +101,14 @@ function base_wav_parser(Input::WavMemoryInput, meta::WavMetadata{Bits, Channels
         dest = Vector{TargetType}(undef, n_frames)
         _process_bits!(dest, data, meta)
         if ismalloc
-            Libc.free(raw_ptr) 
+            Libc.free(raw_ptr)
         end
         return dest, meta.sample_rate
     end
 end
 
-audioread(Input::WavMemoryInput, meta::WavMetadata, parser_function::F = base_wav_parser) where {F<:Function} = parser_function(Input, meta, get_base_type(meta))
-audioread(Input::WavMemoryInput, meta::WavMetadata{Bits, Channels}, native_output::Bool, parser_function::F = base_wav_parser) where {Bits, Channels, F<:Function} = native_output ? audioread(Input, meta) : parser_function(Input, meta, TransportMapping[Bits])
+audioread(Input::WavMemoryInput, meta::WavMetadata{S, Channels}, parser_function::F = base_wav_parser) where {S, Channels, F<:Function} = parser_function(Input, meta, S)
+audioread(Input::WavMemoryInput, meta::WavMetadata{S, Channels}, native_output::Bool, parser_function::F = base_wav_parser) where {S, Channels, F<:Function} = parser_function(Input, meta, native_output ? S : get_transport_type(S))
 function audioread(path::String, output_param::Union{Bool, Type} = false, parser_function::F = base_wav_parser) where {F<:Function}
     raw_vec, raw_ptr = malloc_read(path)
     meta = get_layout_wav(raw_vec)
@@ -103,7 +119,14 @@ function audioread(path::String, output_param::Union{Bool, Type} = false, parser
     end
 end
 
-@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Val{24}, ::Type{ET}, format_tag) where {ET}
+@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Type{Q0f15}, ::Type{ET}) where {ET}
+    s16 = unsafe_load(reinterpret(Ptr{Int16}, ptr))
+    if ET <: AbstractFloat;     return Float32(s16) * (1.0f0 / 32768.0f0)
+    elseif ET <: FixedPoint;    return reinterpret(ET, s16)
+    end
+    return s16 % ET
+end
+@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Type{Q0f23}, ::Type{ET}) where {ET}
     u24 = UInt32(unsafe_load(ptr, 1)) | (UInt32(unsafe_load(ptr, 2)) << 8) | (UInt32(unsafe_load(ptr, 3)) << 16)
     s32 = reinterpret(Int32, u24 << 8) 
     if ET <: Integer;           return s32 % ET
@@ -112,35 +135,31 @@ end
     end
     return s32
 end
-
-@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Val{16}, ::Type{ET}, format_tag) where {ET}
-    s16 = unsafe_load(reinterpret(Ptr{Int16}, ptr))
-    if ET <: AbstractFloat;     return Float32(s16) * (1.0f0 / 32768.0f0)
-    elseif ET <: FixedPoint;    return reinterpret(ET, s16)
+@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Type{Q0f31}, ::Type{ET}) where {ET}
+    s32 = unsafe_load(reinterpret(Ptr{Int32}, ptr))
+    if ET <: AbstractFloat;     return Float32(s32) * (1.0f0 / 2147483648.0f0)
+    elseif ET <: FixedPoint;    return reinterpret(ET, s32)
     end
-    return s16 % ET
+    return s32 % ET
+end
+@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Type{Float32}, ::Type{ET}) where {ET}
+    f32 = unsafe_load(reinterpret(Ptr{Float32}, ptr))
+    if ET <: Float32;           return f32
+    elseif ET <: FixedPoint;    return convert(ET, f32)
+    end
+    return ET(f32) # Fallback
 end
 
-@inline function _read_pcm_sample(ptr::Ptr{UInt8}, ::Val{32}, ::Type{ET}, format_tag) where {ET}
-    u32 = unsafe_load(reinterpret(Ptr{UInt32}, ptr))
-    if ET <: AbstractFloat
-        return format_tag == 3 ? reinterpret(Float32, u32) : Float32(reinterpret(Int32, u32)) * (1.0f0 / 2147483648.0f0)
-    end
-    return reinterpret(Int32, u32) % ET
-end
-
-function _process_bits!(dest::AbstractVector{Sample{nchans, T}}, raw::Vector{UInt8}, meta::WavMetadata{nbits, nchans}) where {nchans, T, nbits}
+function _process_bits!(dest::AbstractVector{Sample{Channels, T}}, raw::Vector{UInt8}, meta::WavMetadata{S, Channels}) where {Channels, T, S}
     GC.@preserve raw begin
-        base_ptr = pointer(raw) + meta.data_offset - 1
-        bps = nbits ÷ 8
-        bpf = nchans * bps
-        f_tag = meta.format_tag
+        base_ptr = pointer(raw) + meta.data_offset
+        bytes_per_sample::Int = S === Q0f23 ? 3 : sizeof(S)
+        bytes_per_frame::Int = Channels * bytes_per_sample
         @inbounds for i in eachindex(dest)
-            frame_ptr = base_ptr + (i - 1) * bpf
-            # Inline the ntuple creation for better performance and readability
-            dest[i] = Sample{nchans, T}(ntuple(ch -> 
-                _read_pcm_sample(frame_ptr + (ch - 1) * bps, Val(nbits), T, f_tag), 
-                Val(nchans))
+            frame_ptr = base_ptr + (i - 1) * bytes_per_frame
+            dest[i] = Sample{Channels, T}(ntuple(ch -> 
+                _read_pcm_sample(frame_ptr + (ch - 1) * bytes_per_sample, S, T), 
+                Val(Channels))
             )
         end
     end
